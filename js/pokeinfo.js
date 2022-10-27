dc9b947ab04e74113f08874cfbde458eb1e4bee5
Lídia Galdino <lidiagabrielly14@gmail.com>
2022-10-27 09:36:39 -0300

    att

--- js/pokeinfo.js
+++ js/pokeinfo.js
@@ -1,7 +1,18 @@
 'use strict'
 
+import { getPokemon } from "./consumo.js";
+
+const creteTitle = (title) => {
+    
+}
+
 const loadPokemon = async (pokemon) => {
 
     const containerInfo = document.getElementById('container-info')
     
+    const data = getPokemon(pokemon)
+
+    const title = creteTitle(data.name)
 }
+
+loadPokemon(localStorage.getItem('pokemon'))
